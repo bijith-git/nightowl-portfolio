14ee67c6b0569fcf7018d3b66fda6d1be8d6590e
Bijith <bijithpn@gmail.com>
2024-02-18 00:19:46 +0530

    service screen hover issue fixed

--- src/components/service.js
+++ src/components/service.js
@@ -29,7 +29,7 @@ export default function Service() {
             </div>
             <div className="mil-complex-text justify-content-center mil-up mil-mb-15">
               <span className="mil-text-image">
-                <img src="img/photo/2.jpg" alt="team" />
+                <img src="/img/photo/2.jpg" alt="team" />
               </span>
               <h2 className="mil-h1 mil-muted mil-center">
                 Unique <span className="mil-thin">Ideas</span>
@@ -40,7 +40,7 @@ export default function Service() {
                 For Your <span className="mil-thin">Business.</span>
               </h2>
               <a
-                href="services.html"
+                href="/services"
                 className="mil-services-button mil-button mil-arrow-place"
               >
                 <span>What we do</span>
@@ -49,59 +49,54 @@ export default function Service() {
           </div>
           <div className="row mil-services-grid m-0">
             <div className="col-md-6 col-lg-3 mil-services-grid-item p-0">
-              <a href="service.html" className="mil-service-card-sm mil-up">
+              <a href="/services" className="mil-service-card-sm mil-up">
                 <h5 className="mil-muted mil-mb-30">
                   Branding and <br />
                   Identity Design
                 </h5>
-                <div className="mil-light-soft mil-mb-30">
+                <p className="mil-light-soft mil-mb-30">
                   Our creative agency is a team of professionals focused on
                   helping your brand grow.
-                </div>
+                </p>
                 <div className="mil-button mil-icon-button-sm mil-arrow-place" />
               </a>
             </div>
             <div className="col-md-6 col-lg-3 mil-services-grid-item p-0">
-              <a href="service.html" className="mil-service-card-sm mil-up">
+              <a href="/services" className="mil-service-card-sm mil-up">
                 <h5 className="mil-muted mil-mb-30">
                   Website Design <br />
                   and Development
                 </h5>
-                <div className="mil-light-soft mil-mb-30">
-                  Crucial for designing an engaging, functional, and intuitive
-                  online presence that harmonizes with your brand&apos;s
-                  essence.An agile and comprehensive approach to developing and
-                  maintaining cutting-edge websites and web applications
-                </div>
+                <p className="mil-light-soft mil-mb-30">
+                  An agile and comprehensive approach to developing and
+                  maintaining web applications.
+                </p>
                 <div className="mil-button mil-icon-button-sm mil-arrow-place" />
               </a>
             </div>
             <div className="col-md-6 col-lg-3 mil-services-grid-item p-0">
-              <a href="service.html" className="mil-service-card-sm mil-up">
+              <a href="/services" className="mil-service-card-sm mil-up">
                 <h5 className="mil-muted mil-mb-30">
                   Advertisement and <br />
                   Digital Marketing
                 </h5>
-                <div className="mil-light-soft mil-mb-30">
-                  Advertisement production focuses on crafting impactful content
-                  to promote products, services, or brands effectively. Digital
-                  marketing transcends borders, connecting you with a diverse,
-                  global audience through innovative strategies
-                </div>
+                <p className="mil-light-soft mil-mb-30">
+                  Focuses on crafting impactful content to promote
+                  products,brands effectively.
+                </p>
                 <div className="mil-button mil-icon-button-sm mil-arrow-place" />
               </a>
             </div>
             <div className="col-md-6 col-lg-3 mil-services-grid-item p-0">
-              <a href="service.html" className="mil-service-card-sm mil-up">
+              <a href="/services" className="mil-service-card-sm mil-up">
                 <h5 className="mil-muted mil-mb-30">
                   Photography and <br />
                   Videography
                 </h5>
-                <div className="mil-light-soft mil-mb-30">
-                  We capture the essence of all your special events, from
-                  weddings and parties to corporate seminars and exhibitions,
-                  with our versatile photography services
-                </div>
+                <p className="mil-light-soft mil-mb-30">
+                  We capture the essence of all your special events with our
+                  versatile photography services
+                </p>
                 <div className="mil-button mil-icon-button-sm mil-arrow-place" />
               </a>
             </div>
