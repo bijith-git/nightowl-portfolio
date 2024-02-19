c0b3d03bb696011ff0c3d0e8b82f67f99531943e
Bijith <bijithpn@gmail.com>
2024-02-19 21:13:08 +0530

    google font tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,12 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
