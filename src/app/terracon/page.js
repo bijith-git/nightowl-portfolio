a97c990b32738a4594cd246cb1cb9b198586ae84
Bijith <bijithpn@gmail.com>
2024-03-06 20:47:28 +0530

    Terracon page updated

--- src/app/terracon/page.js
+++ src/app/terracon/page.js
@@ -1,4 +1,330 @@
+import MainLayout from "@/layout/mainLayout";
+
 const Terracon = () => {
-  return <div>test</div>;
+  return (
+    <MainLayout>
+      <div className="mil-inner-banner">
+        <div className="mil-banner-content mil-up">
+          <div className="mil-animation-frame">
+            <div
+              className="mil-animation mil-position-4 mil-dark mil-scale"
+              data-value-1={6}
+              data-value-2="1.4"
+            />
+          </div>
+          <div className="container">
+            <ul className="mil-breadcrumbs mil-mb-60">
+              <li>
+                <a href="/">Homepage</a>
+              </li>
+              <li>
+                <a href="/portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="">Terracon</a>
+              </li>
+            </ul>
+            <h1 className="mil-mb-60">
+              <span className="mil-thin">Terracon </span>
+            </h1>
+            <a
+              href="#project"
+              className="mil-link mil-dark mil-arrow-place mil-down-arrow"
+            >
+              <span>Read more</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <section id="project">
+        <div className="container mil-p-120-120">
+          <div className="swiper-container mil-2-slider mil-up">
+            <div className="swiper-wrapper">
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/1.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/1.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/2.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/2.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/3.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/3.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/4.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/4.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/5.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/5.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/6.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/6.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/6.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/6.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/7.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/7.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/8.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/8.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/9.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/9.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/10.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/10.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/11.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/11.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/12.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/12.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/13.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/13.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>{" "}
+              <div className="swiper-slide">
+                <div className="mil-image-frame mil-vertical mil-drag">
+                  <img src="img/works/7/14.jpg" alt="image" />
+                  <a
+                    data-fancybox="gallery"
+                    data-no-swup=""
+                    href="img/works/7/14.jpg"
+                    className="mil-zoom-btn"
+                  >
+                    <img src="img/icons/zoom.svg" alt="zoom" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mil-info mil-up">
+            <div>
+              Client: &nbsp;<span className="mil-dark">Terracon </span>
+            </div>
+            <div>
+              Date: &nbsp;<span className="mil-dark">February 2024</span>
+            </div>
+            <div>
+              Photography: &nbsp;<span className="mil-dark">Sreerag</span>
+            </div>
+          </div>
+          <div className="row justify-content-center mil-center mil-p-120-120">
+            <div className="col-lg-8">
+              <h3 className="mil-up mil-mb-60">
+                Elegance Captured <br />
+                Explore Our Exquisite Collection of Tile and Home Décor Marvels.
+              </h3>
+              <p className="mil-text-lg mil-up">
+                Immerse yourself in a world of sophistication with our stunning
+                product photography showcasing a diverse array of tiles,
+                marbles, and home decoration items. Each image is a visual
+                symphony, meticulously crafted to highlight the intricate
+                details and timeless beauty of our offerings. From luxurious
+                marble surfaces to eye-catching tiles and curated home décor
+                accents, our collection is a testament to style, quality, and
+                the art of transforming spaces. Discover the perfect blend of
+                functionality and aesthetics, inviting you to envision your
+                dream home in every pixel. Elevate your interior design journey
+                with our captivating product gallery – where elegance meets
+                inspiration.
+              </p>
+            </div>
+          </div>
+          <div className="mil-image-frame mil-horizontal mil-up mil-mb-120">
+            <img src="img/works/7/7.jpg" alt="image" />
+            <a
+              data-fancybox="gallery"
+              data-no-swup=""
+              href="img/works/7/7.jpg"
+              className="mil-zoom-btn"
+            >
+              <img src="img/icons/zoom.svg" alt="zoom" />
+            </a>
+          </div>
+          <div className="mil-works-nav mil-up">
+            <a
+              href="project-3.html"
+              className="mil-link mil-dark mil-arrow-place mil-icon-left"
+            >
+              <span>Prev project</span>
+            </a>
+            <a href="portfolio-1.html" className="mil-link mil-dark">
+              <span>All projects</span>
+            </a>
+            <a
+              href="project-5.html"
+              className="mil-link mil-dark mil-arrow-place"
+            >
+              <span>Next project</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="mil-soft-bg">
+        <div className="container mil-p-120-120">
+          <div className="row">
+            <div className="col-lg-10">
+              <span className="mil-suptitle mil-suptitle-right mil-suptitle-dark mil-up">
+                Looking to make your mark? We&apos;ll help you turn <br />
+                your project into a success story.
+              </span>
+            </div>
+          </div>
+          <div className="mil-center">
+            <h2 className="mil-up mil-mb-60">
+              Ready to bring your
+              <span className="mil-thin">ideas to</span> life? <br />
+              We&apos;re <span className="mil-thin">here to help</span>
+            </h2>
+            <div className="mil-up">
+              <a href="contact.html" className="mil-button mil-arrow-place">
+                <span>Contact us</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </MainLayout>
+  );
 };
 export default Terracon;
