2bde7655f126ea5397649ba68fc689c298a1d6c3
Bijith <bijith.sigosoft@gmail.com>
2024-03-23 16:52:05 +0530

    about us and client image udpated

--- src/components/clients.js
+++ src/components/clients.js
@@ -5,63 +5,185 @@ export default function Clients() {
         <div className="swiper-container mil-infinite-show mil-up">
           <div className="swiper-wrapper">
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  objectFit: "cover",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-01.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              >
                 <img src="img/partners/logos-02.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  objectFit: "cover",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-03.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  objectFit: "cover",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-04.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-05.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-06.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-07.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-08.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-09.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-10.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-11.png" alt="logo" />
               </div>
             </div>
             <div className="swiper-slide">
-              <div className="mil-partner-frame">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
                 <img src="img/partners/logos-12.png" alt="logo" />
+              </div>
+            </div>
+            <div className="swiper-slide">
+              <div
+                className="mil-partner-frame"
+                style={{
+                  display: "flex",
+                  objectFit: "cover",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                <img src="img/partners/logos-13.png" alt="logo" />
               </div>
             </div>
           </div>
