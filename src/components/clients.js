70366a5f6b7926b32249e0266007c28f39211982
bijith <bijithpn@gmail.com>
2024-05-15 23:13:58 +0530

    "Replace static partner logo code with dynamic image import and update CSS for image sizing"
    This diff removes the static code for 13 partner logos and replaces it with a dynamic array of image URLs. It also updates

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,204 +1,23 @@
 export default function Clients() {
+  const imageUrls = [...Array(13).keys()].map(
+    (index) => `img/partners/logos-${index + 1}.png`
+  );
   return (
     <div className="mil-soft-bg">
       <div className="container mil-p-0-120">
         <div className="swiper-container mil-infinite-show mil-up">
           <div className="swiper-wrapper">
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  objectFit: "cover",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-01.png" alt="logo" />
+            {imageUrls.map((imageUrl, index) => (
+              <div className="swiper-slide" key={index}>
+                <div className="mil-partner-frame">
+                  <img
+                    src={imageUrl}
+                    alt={`logo-${index}`}
+                    className="mil-partner-img"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "cover",
-                }}
-              >
-                <img src="img/partners/logos-02.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  objectFit: "cover",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-03.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  objectFit: "cover",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-04.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-05.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-06.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-07.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-08.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-09.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-10.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-11.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-12.png" alt="logo" />
-              </div>
-            </div>
-            <div className="swiper-slide">
-              <div
-                className="mil-partner-frame"
-                style={{
-                  display: "flex",
-                  objectFit: "cover",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
-                <img src="img/partners/logos-13.png" alt="logo" />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
